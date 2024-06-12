b1c15781fbaa850fd3062bea3af92a789002b604
alondo1 <alondo1@gmail.com>
2024-06-12 19:22:45 -0400

    Route render

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route exact path="/" element={<MainPage />} />{" "}
+          <Route exact path="/" render={<MainPage />} />{" "}
         </Routes>
     </BrowserRouter>
   );
